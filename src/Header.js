9a0088fc90bc495f3dd310ab05a7dc69e372f429
RichardZhangCS <richardzhang1303@gmail.com>
2021-12-28 10:29:30 -0600

    Made navbar collapsable in small screens

--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,42 @@
 import "./Header.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 export default function Header({ sectionUnderline }) {
   return (
-    <header>
-      <nav className="nav-bar transparent d-flex align-items-center justify-content-between w-100 px-5 shadow flex-column flex-sm-row">
-        <div
-          className={
-            "navbar-item" + (sectionUnderline === "home" ? " selected" : "")
-          }
-        >
-          <a href="#home">HOME</a>
-        </div>
-        <div
-          className={
-            "navbar-item" + (sectionUnderline === "projects" ? " selected" : "")
-          }
-        >
-          <a href="#projects">PROJECTS</a>
-        </div>
-        <div
-          className={
-            "navbar-item" + (sectionUnderline === "about" ? " selected" : "")
-          }
-        >
-          <a href="#about">ABOUT</a>
-        </div>
-      </nav>
-    </header>
+    <Navbar expand="sm" className="transparent navbar-dark nav-bar w-100 p-0">
+      <Container
+        fluid
+        className="h-100 align-items-md-center align-items-start pt-md-0 pt-2"
+      >
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="justify-content-between w-100">
+            <div
+              className={
+                "navbar-item" + (sectionUnderline === "home" ? " selected" : "")
+              }
+            >
+              <Nav.Link href="#home">HOME</Nav.Link>
+            </div>
+            <div
+              className={
+                "navbar-item" +
+                (sectionUnderline === "projects" ? " selected" : "")
+              }
+            >
+              <Nav.Link href="#projects">PROJECTS</Nav.Link>
+            </div>
+            <div
+              className={
+                "navbar-item" +
+                (sectionUnderline === "about" ? " selected" : "")
+              }
+            >
+              <Nav.Link href="#about">ABOUT</Nav.Link>
+            </div>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
