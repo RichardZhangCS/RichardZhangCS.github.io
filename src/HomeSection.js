872d9d391776d3b8c86e75d65891b92e1d775564
RichardZhangCS <richardzhang1303@gmail.com>
2021-12-27 19:47:12 -0600

    Added social media buttons

--- src/HomeSection.js
+++ src/HomeSection.js
@@ -9,8 +9,34 @@ export default function HomeSection() {
             RICHARD ZHANG
           </h1>
           <p className="h2 ps-1" id="title">
-            Full Stack Developer
+            {"CS Student & Full Stack Developer"}
           </p>
+          <div className="icon-container">
+            <a
+              href="https://www.linkedin.com/in/richardzhang-rz/"
+              className="fa fa-linkedin text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+            <a
+              href="https://github.com/RichardZhangCS/"
+              className="fa fa-github text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+            <a
+              href="tel:832-359-4651"
+              className="fa fa-mobile text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+            <a
+              href="mailto: richardzhang1303@gmail.com"
+              className="fa fa-envelope text-white"
+              target="_blank"
+              rel="noopener noreferrer"
+            ></a>
+          </div>
         </div>
         <div className="w-50 h-100 d-flex align-items-center justify-content-center">
           <img src={headShot} alt="Richard Zhang" id="headshot" />
